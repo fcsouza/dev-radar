0360bc32769b40dd963747b33afd9def0ff41e05
fcsouza <fcsouza18@hotmail.com>
2020-08-28 21:04:32 -0300

    add dev controller

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,84 @@
+const axios = require("axios");
+const Dev = require("../models/Dev");
+const parseStringAsArray = require("../utils/parseStringAsArray");
+const { findConnections, sendMessage } = require("../websocket");
+
+module.exports = {
+  async index(request, response) {
+    const devs = await Dev.find();
+
+    return response.json(devs);
+  },
+
+  async store(request, response) {
+    const { github_username, techs, latitude, longitude } = request.body;
+
+    let dev = await Dev.findOne({ github_username });
+
+    if (!dev) {
+      const apiResponse = await axios.get(
+        `https://api.github.com/users/${github_username}`
+      );
+
+      const { name = login, avatar_url, bio } = apiResponse.data;
+
+      const techsArray = parseStringAsArray(techs);
+
+      const location = {
+        type: "Point",
+        coordinates: [longitude, latitude]
+      };
+
+      dev = await Dev.create({
+        name,
+        github_username,
+        bio,
+        avatar_url,
+        techs: techsArray,
+        location
+      });
+
+      const sendSocketMessageTo = findConnections(
+        { latitude, longitude },
+        techsArray
+      );
+
+      sendMessage(sendSocketMessageTo, "new-dev", dev);
+    }
+
+    return response.json(dev);
+  },
+
+  async update(request, response) {
+    const { name, bio, avatar_url, techs, latitude, longitude } = request.body;
+
+    const { github_username } = request.params;
+
+    const dev = await Dev.findOne({ github_username });
+
+    const techsArray = parseStringAsArray(techs);
+
+    const location = {
+      type: "Point",
+      coordinates: [longitude, latitude]
+    };
+
+    await dev.update({
+      name,
+      bio,
+      avatar_url,
+      techs: techsArray,
+      location
+    });
+
+    return response.json(request.body);
+  },
+
+  async destroy(request, response) {
+    const { github_username } = request.params;
+
+    await Dev.remove({ github_username });
+
+    return response.json({});
+  }
+};
