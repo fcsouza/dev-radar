97c1f6de82c20886bf09da2d8eb8385bf1f4df15
fcsouza <fcsouza18@hotmail.com>
2020-08-28 21:09:16 -0300

    include starter

---
+++ backend/src/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const http = require("http");
+
+const routes = require("./routes");
+const { setupWebsocket } = require("./websocket");
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.set("useCreateIndex", true);
+
+mongoose.connect(
+  `${process.env.MONGO}`,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  }
+);
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
